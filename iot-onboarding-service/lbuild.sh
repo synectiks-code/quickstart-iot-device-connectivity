adee8c3b85c1fff1594ab041c1d0feace46a7145
Geoffroy Rollat <rollatgr@amazon.com>
2021-03-15 12:38:59 -0400

    fixed build script for lambda

--- iot-onboarding-service/lbuild.sh
+++ iot-onboarding-service/lbuild.sh
@@ -2,6 +2,8 @@
 
 env=$1
 bucket=$2
+#update this variable to specify the name of your loval env
+LOCAL_ENV_NAME=dev
 
 echo "**********************************************"
 echo "* IOT Onboarding Service deployement for env '$env' "
@@ -16,21 +18,43 @@ echo "1-Setting up environement"
 export GOPATH=$(pwd)
 echo "GOPATH set to $GOPATH"
 echo "Instaling dependencies"
+
+if [ $env == $LOCAL_ENV_NAME ]
+    then
+    echo "Local environement build"
+    else
+    sh ./env.sh $env
+fi
 echo "2-Unit Testing"
 rm main main.zip
+#in local we do not set the GOOS env variable to have a MACOS build
+if [ $env == $LOCAL_ENV_NAME ]
+    then
+    echo "2.1-Local Unit Testing"
+    go build -o main src/main/main.go
+    sh ./test.sh
+    rc=$?
+    if [ $rc -ne 0 ]; then
+      echo "Existing Build with status $rc" >&2
+      exit $rc
+    fi
+fi
 echo "3-Building application Deployement"
 export GOOS=linux
 go build -o main src/main/main.go
-echo "3.1-Unit Testing"
-sh ./test.sh
-rc=$?
-if [ $rc -ne 0 ]; then
-  echo "Existing Build with status $rc" >&2
-  exit $rc
+if [ $env != $LOCAL_ENV_NAME ]; then
+    echo "3.1-Unit Testing"
+    sh ./test.sh
+    rc=$?
+    if [ $rc -ne 0 ]; then
+      echo "Existing Build with status $rc" >&2
+      exit $rc
+    fi
 fi
-
 zip main.zip main
 echo "4-Deploying to Lambda"
 sh push.sh $env $bucket
-cd e2e && sh test.sh $env && cd ..
+if [ $env == $LOCAL_ENV_NAME ]; then
+  cd e2e && sh test.sh $env && cd ..
+fi
 fi
