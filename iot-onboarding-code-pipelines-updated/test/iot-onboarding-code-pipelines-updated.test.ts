1a04471556c328357576576d9b6effcded2bd0c7
Jasmin Makasana <jasmakasana@gmail.com>
2023-07-25 11:15:33 +0530

    -----> iot-onboarding-code-pipelines-updated create

---
+++ iot-onboarding-code-pipelines-updated/test/iot-onboarding-code-pipelines-updated.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IotOnboardingCodePipelinesUpdated from '../lib/iot-onboarding-code-pipelines-updated-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iot-onboarding-code-pipelines-updated-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IotOnboardingCodePipelinesUpdated.IotOnboardingCodePipelinesUpdatedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
